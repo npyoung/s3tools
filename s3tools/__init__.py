87a46d7f5600e53d9b5af093bb6a3919419b01aa
Noah Young <noahyoung@system1.bio>
2019-01-10 21:51:35 +0000

    full S3 URLs parsed, can provided default bucket instead of hardcoding

---
+++ s3tools/__init__.py
@@ -0,0 +1,2 @@
+from .s3tools import *
+__all__ = [s3open, get_s3_img, put_s3_img]
--- s3tools/s3tools.py
+++ s3tools/s3tools.py
@@ -1,22 +1,49 @@
 from contextlib import contextmanager
 from glob import glob
 from io import BytesIO, StringIO
+import os
 import warnings
 
 import boto3
 from tifffile import imread, imsave
 
 
-BUCKET = 'xlfm-dcon'
+_bucket = None
 _s3 = boto3.resource('s3')
+
+def set_bucket(bucket):
+    _bucket = bucket
+
+def default_bucket():
+    if _bucket:
+        return _bucket
+    
+    try:
+        return os.environ['S3TOOLS_BUCKET']
+    except KeyError as e:
+        print("No bucket provided and neither `s3tools.set_bucket()` nor `S3TOOLS_BUCKET` have been set.")
+        print("Please provide a way to determine the S3 bucket")
+        raise e
+    
+
+def parse_url(url):
+    # Full S3 URL provided
+    if url[:5] == "s3://":
+        parts = url[5:].split("/", maxsplit=1)
+        bucket = parts[0]
+        key = parts[1]
+    # Fall back on default bucket
+    else:
+        bucket = default_bucket()
+        key = url
+    return _s3.Bucket(bucket), key
 
 
 def get_keys(prefix):
-    bucket = _s3.Bucket(BUCKET)
-
+    bucket, prefix = parse_url(prefix)
     return [o.key for o in bucket.objects.filter(Prefix=prefix)]
 
-def s3open(key, mode='rb'):
+def s3open(url_or_key, mode='rb'):
     if mode == 'rb':
         return S3ReadBuffer(key)
     elif mode == 'wb':
@@ -25,8 +52,8 @@ def s3open(key, mode='rb'):
         raise ValueError("Only modes 'rb' and 'wb' supported")
 
 @contextmanager
-def S3ReadBuffer(key):
-    bucket = _s3.Bucket(BUCKET)
+def S3ReadBuffer(url_or_key):
+    bucket, key = parse_url(url_or_key)
     
     with BytesIO() as buff:
         bucket.download_fileobj(key, buff)
@@ -34,8 +61,8 @@ def S3ReadBuffer(key):
         yield buff
         
 @contextmanager
-def S3WriteBuffer(key):
-    bucket = _s3.Bucket(BUCKET)
+def S3WriteBuffer(url_or_key):
+    bucket, key = parse_url(url_or_key)
     
     with BytesIO() as buff:
         yield buff
